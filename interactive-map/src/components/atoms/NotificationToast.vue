ec3cfa09d27be9ac7eb00258b95ad537f5e145b9
jot4jj <jsilvafreitas25@gmail.com>
2025-11-19 09:58:48 -0300

    fix: Garante a inserção do conteúdo do componente

--- interactive-map/src/components/atoms/NotificationToast.vue
+++ interactive-map/src/components/atoms/NotificationToast.vue
@@ -2,5 +2,6 @@
   <p
     class="flex absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4 py-2 rounded-lg shadow-lg z-30 bg-[#1e293b] text-gray-200"
   >
+  <slot></slot>
   </p>
 </template>

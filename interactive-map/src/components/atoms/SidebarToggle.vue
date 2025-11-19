4c2d64c9a6d70d3f2f0a0e603bbcad47d11965a3
jot4jj <jsilvafreitas25@gmail.com>
2025-11-19 11:07:09 -0300

    style: Altera a cor do SidebarToggle

--- interactive-map/src/components/atoms/SidebarToggle.vue
+++ interactive-map/src/components/atoms/SidebarToggle.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed w-10 top-4 left-4 bg-[#293447] z-30 hover:cursor-pointer rounded-md shadow-lg flex items-center justify-center hover:scale-110"
+    class="fixed w-10 top-4 left-4 bg-[#1E293B] z-30 hover:cursor-pointer rounded-md shadow-lg flex items-center justify-center hover:scale-110"
     @click="handleToggle"
   >
   <img src="../../assets/img/menu.png" alt="menu">

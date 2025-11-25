fd127c0994cc5d9924c493264740cca56cd9e093
jot4jj <jsilvafreitas25@gmail.com>
2025-11-25 16:35:55 -0300

    feat(search-input): Adiciona funcao de busca e de limpeza do input

--- interactive-map/src/components/atoms/SearchAddress.vue
+++ interactive-map/src/components/atoms/SearchAddress.vue
@@ -1,34 +1,61 @@
 <template>
-<div class="w-full flex mx-4 p-2 bg-[#1E293B] text-white rounded-md">
+<div class="flex gap-4 mx-4 p-2 bg-[#1E293B] text-white rounded-md ">
     <input
         v-model="address"
         type="text"
         placeholder="Digite um endereço..."
         class="w-full p-2 rounded bg-gray-800 border border-gray-700 outline-none"
     />
-    <BaseButton
-        @click="search"    
-    >
-        Buscar
-    </BaseButton>
+
+    <div class="flex gap-2 justify-center p-2 items-center">
+        <img
+            src="../../assets/img/search.png"
+            alt="search"
+            @click="search"
+            class="w-8 hover:cursor-pointer"
+        >
+
+        <img
+            src="../../assets/img/close.png"
+            alt="close"
+            @click="close"
+            class="w-11 hover:cursor-pointer"
+        >
+    </div>
+
+    <NotificationToast v-if="showInvalidAddress">
+        Digite um endereço válido
+    </NotificationToast>
+
+    <NotificationToast v-if="showAddressNotFound">
+        Endereço não encontrado
+    </NotificationToast>
+
+    <LoadingSpinner v-if="showLoading"/>
 </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import BaseButton from "./BaseButton.vue";
+import NotificationToast from "./NotificationToast.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
 
 const props = defineProps({
     map: Object,
 });
-
 const emit = defineEmits(["found"]);
-
 const address = ref("");
+const showInvalidAddress = ref(false)
+const showAddressNotFound = ref(false)
+const showLoading = ref(false)
 
 async function search() {
+    showLoading.value = true
+
     if (!address.value.trim()) {
-        alert("Digite um endereço.");
+        showInvalidAddress.value = true
+        setTimeout(() => (showInvalidAddress.value = false), 3000)
+        showLoading.value = false
         return;
     }
 
@@ -41,7 +68,9 @@ async function search() {
     const data = await response.json();
 
     if (data.length === 0) {
-        alert("Endereço não encontrado!");
+        showAddressNotFound.value = true
+        setTimeout(() => (showAddressNotFound.value = false), 3000)
+        showLoading.value = false
         return;
     }
 
@@ -52,13 +81,17 @@ async function search() {
         lat,
         lon,
         address: address.value
-    });
+    }); 
 
-    console.log(address.value)
+    showLoading.value = false
 
     } catch (error) {
         console.error("Erro ao buscar endereço:", error);
     }
+}
+
+function close() {
+    address.value = ''
 }
 
 document.addEventListener('keydown', (e) => {
